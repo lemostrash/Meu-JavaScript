ddd625a180742b3385462a58d6ee6d1110aed0b6
Lemostrash <andrelemosplay@gmail.com>
2022-03-08 09:19:32 -0300

    JS

---
+++ Javascript/JS/main.js
@@ -0,0 +1,12 @@
+
+
+var nome = "André de Lemos";
+var idade = 20;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+console.log(nome)
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão", "Brasil"));
